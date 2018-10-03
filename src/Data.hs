7ef43d579204ca50afcccfb6c78f0e73fce28dff
raydabbah <raydabbah@gmail.com>
2018-10-03 01:28:40 -0400

    refactor grid and languages to new file

---
+++ src/Data.hs
@@ -0,0 +1,28 @@
+module Data (
+      grid
+    , languages
+) where
+
+grid = [ "__C______R____M"
+        ,"_________U____L"
+        ,"_________B____E"
+        ,"_HASKELL_Y____O"
+        ,"____PHP______C_"
+        ,"E___________A_J"
+        ,"RL_________M__A"
+        ,"L_I___LREP_P__V"
+        ,"A__X________S_A"
+        ,"N___I________I_"
+        ,"G____R________L"]
+
+languages = ["OCAM"
+            ,"ELIXIR"
+            ,"LISP"
+            , "PERL"
+            , "HASKELL"
+            , "RUBY"
+            , "JAVA"
+            , "PHP"
+            , "ERLANG"
+            , "JAVA"
+            , "ELM"]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,6 @@
 module Lib
     (
      showGrid
-    , grid
-    , languages
     , findInLine
     , findWord
     , findWords
@@ -49,21 +47,3 @@ findWord grid word =
 findWords grid words =
     let foundWords = map (findWord grid) words
     in catMaybes foundWords
-
--- onlyJusts = catMaybes findWords
-
--- containedInGrid
-
-grid = [ "__C______R____M"
-        ,"_________U____L"
-        ,"_________B____E"
-        ,"_HASKELL_Y____O"
-        ,"____PHP______C_"
-        ,"E___________A_J"
-        ,"RL_________M__A"
-        ,"L_I___LREP_P__V"
-        ,"A__X________S_A"
-        ,"N___I________I_"
-        ,"G____R________L"]
-
-languages = ["OCAM","ELIXIR","LISP","CSHARP", "PERL", "PYTHON", "CPLUSPLUS", "NODE", "HASKELL", "RUBY", "JAVA", "PHP", "ERLANG", "JAVA", "ELM"]
