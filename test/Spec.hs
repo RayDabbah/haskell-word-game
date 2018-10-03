691379434fd29b12e96599de2b0d7bf2155c6aa4
raydabbah <raydabbah@gmail.com>
2018-10-03 01:28:53 -0400

    testing grid

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,24 @@
-import Test.Hspec
-import Lib
+import           Data
+import           Lib
+import           Test.Hspec
 
-main :: IO ()
-main = hspec $ do 
-    describe "This is how to do a test" $ do
-        it "Should Run tests!" $ do
-            someString `shouldBe` "someSuperFunc"
+main = hspec $ do
+    describe "showGrid" $ do
+        it "This should concatenate the grid" $ do
+            (unlines ["abc", "def", "ghc"]) `shouldBe` "abc\ndef\nghc\n"
+
+    describe "findWord" $ do
+        it "Should Find a word in the grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "RUBY" `shouldBe` Just "RUBY"
+            findWord grid "PHP" `shouldBe` Just "PHP"
+            findWord grid "ELIXIR" `shouldBe` Just "ELIXIR"
+        it "Should not find a word that doesn't exist" $ do
+            findWord grid "HAPPY" `shouldBe` Nothing
+
+    describe "findWords" $ do 
+        it "Should find all the words in the list" $ do
+            findWords grid languages `shouldBe` languages
+        it "Should not find  words not in the list" $ do
+            findWords grid ["APPLES", "HEBREW"] `shouldBe` []
+
